138f945b18b89b8caa45269b9c41226e41b24fa6
sigmdel <michel@sigmdel.ca>
2023-06-13 18:46:57 -0300

    processUDPPacket() does not need to be public

--- Firmware/src/ntp_server.h
+++ Firmware/src/ntp_server.h
@@ -6,8 +6,9 @@ class NTP_Server {
 public:
     NTP_Server( );
     ~NTP_Server();
-    
+
     bool begin(uint16_t port , uint32_t(*fnc_getutc_time)(void) , uint32_t(*fnc_get_subsecond)(void) );
+private:    
     static void processUDPPacket(AsyncUDPPacket& packet);
       
 };
